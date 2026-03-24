6b534d56c1a8edefa12f9313c39bd646b6636d2f
Tanaydesh08 <assassincyber7@gmail.com>
2026-03-24 21:17:11 +0530

    implemented password encryption and decryption

--- mentor-platform/mentor-platform/src/main/java/com/tanaydeshmukh/mentor_platform/controller/UserController.java
+++ mentor-platform/mentor-platform/src/main/java/com/tanaydeshmukh/mentor_platform/controller/UserController.java
@@ -4,8 +4,10 @@ import com.tanaydeshmukh.mentor_platform.dto.LoginRequest;
 import com.tanaydeshmukh.mentor_platform.dto.UserDTO;
 import com.tanaydeshmukh.mentor_platform.entity.User;
 import com.tanaydeshmukh.mentor_platform.repository.UserRepository;
+import com.tanaydeshmukh.mentor_platform.security.JwtUtil;
 import com.tanaydeshmukh.mentor_platform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +20,10 @@ public class UserController {
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtUtil jwtUtil;
 
     //create user
     @PostMapping
@@ -32,14 +38,27 @@ public class UserController {
     }
 
 
-    // 🔐 Login API (ADD THIS)
+//    // 🔐 Login API (ADD THIS)
+//    @PostMapping("/login")
+//    public String login(@RequestBody LoginRequest request) {
+//        return userService.login(request.getEmail(), request.getPassword());
+//    }
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request) {
-        return userService.login(request.getEmail(), request.getPassword());
+    public String login(@RequestBody User loginRequest) {
+
+             User user = userRepository.findByEmail(loginRequest.getEmail())
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+             if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                     throw new RuntimeException("Invalid password");
+            }
+
+             return jwtUtil.generateToken(user.getEmail());
     }
 
     @PostMapping("/register")
     public String register(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return "User registered successfully!";
     }
